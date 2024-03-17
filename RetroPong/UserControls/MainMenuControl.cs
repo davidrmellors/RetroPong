a231346493b82ad69d7f356bf83ad45dd1bb85ef
David Mellors <davidroymellors@gmail.com>
2024-03-17 22:25:30 +0200

    Create MainMenu UI

--- RetroPong/UserControls/MainMenuControl.cs
+++ RetroPong/UserControls/MainMenuControl.cs
@@ -12,6 +12,11 @@ namespace RetroPong.UserControls
 {
     public partial class MainMenuControl : UserControl
     {
+        private Button btnSinglePlayer;
+        private Button btnMultiplayer;
+        private Button btnSettings;
+        private Button btnQuit;
+
         public event EventHandler PlaySinglePlayer;
         public event EventHandler PlayMultiplayer;
         public event EventHandler OpenSettings;
@@ -20,16 +25,26 @@ namespace RetroPong.UserControls
         public MainMenuControl()
         {
             InitializeComponent();
-            // Add your buttons here and wire up the events.
+            CreateUI();
         }
 
-        // Example button click event
-        private void btnSinglePlayer_Click(object sender, EventArgs e)
+        private void CreateUI()
         {
-            PlaySinglePlayer?.Invoke(this, EventArgs.Empty);
-        }
+            btnSinglePlayer = new Button { Text = "Single Player", Size = new Size(200, 40), Location = new Point(150, 60) };
+            btnMultiplayer = new Button { Text = "Multiplayer", Size = new Size(200, 40), Location = new Point(150, 120) };
+            btnSettings = new Button { Text = "Settings", Size = new Size(200, 40), Location = new Point(150, 180) };
+            btnQuit = new Button { Text = "Quit Game", Size = new Size(200, 40), Location = new Point(150, 240) };
 
-        // Implement other buttons similarly...
+            btnSinglePlayer.Click += (sender, e) => PlaySinglePlayer?.Invoke(this, EventArgs.Empty);
+            btnMultiplayer.Click += (sender, e) => PlayMultiplayer?.Invoke(this, EventArgs.Empty);
+            btnSettings.Click += (sender, e) => OpenSettings?.Invoke(this, EventArgs.Empty);
+            btnQuit.Click += (sender, e) => QuitGame?.Invoke(this, EventArgs.Empty);
+
+            this.Controls.Add(btnSinglePlayer);
+            this.Controls.Add(btnMultiplayer);
+            this.Controls.Add(btnSettings);
+            this.Controls.Add(btnQuit);
+        }
     }
 
 }
